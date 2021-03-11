65491f0d29f80fcb0b1eba94ffb83827d07d9223
Kinnell Shah <kinnell@gmail.com>
2021-03-11 10:10:43 -0500

    Silence deprecation warning

--- lib/ractive/rails/engine.rb
+++ lib/ractive/rails/engine.rb
@@ -3,8 +3,8 @@ module Ractive
     class Engine < ::Rails::Engine
       config.before_initialize do |app|
         Sprockets::Engines
-        Sprockets.register_engine('.rac', Tilt)
-        Sprockets.register_engine('.ractive', Tilt)
+        Sprockets.register_engine('.rac', Tilt, silence_deprecation: true)
+        Sprockets.register_engine('.ractive', Tilt, silence_deprecation: true)
       end
     end
   end
