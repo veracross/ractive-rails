688e3547c489c5e0aca700a3faf5c29801de99fd
Henry Thach <henry.thach@veracross.com>
2022-09-28 12:15:59 -0400

    Support for Sprockets 4

--- lib/ractive/rails/engine.rb
+++ lib/ractive/rails/engine.rb
@@ -2,8 +2,20 @@ module Ractive
   module Rails
     class Engine < ::Rails::Engine
       config.before_initialize do |app|
-        Sprockets.register_mime_type 'text/ractive', extensions: ['.rac', '.ractive']
-        Sprockets.register_preprocessor 'text/ractive', Tilt
+        if Sprockets.respond_to?(:register_transformer)
+          # Sprockets 4
+          Sprockets.register_mime_type 'text/ractive', extensions: ['.rac', '.rac.erb', '.ractive']
+          Sprockets.register_transformer 'text/ractive', 'application/javascript', Tilt
+        elsif Sprockets.respond_to?(:register_engine)
+          # Sprockets 2, 3 (old)
+          args = ['.rac', Tilt]
+          args << { mime_type: 'application/javascript', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+          Sprockets.register_engine(*args)
+
+          args = ['.ractive', Tilt]
+          args << { mime_type: 'application/javascript', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+          Sprockets.register_engine(*args)
+        end
       end
     end
   end
